27f84301b934103635c3a1bc3e15d2c34988fcea
komalpreethora <36226555+komalpreethora@users.noreply.github.com>
2018-03-03 19:19:12 +0530

    Add files via upload

---
+++ Ucan_FE-master/js/homeController.js
@@ -0,0 +1,95 @@
+myApp.controller('home',function($scope, $log, $http, $q){
+  //--Initialisation
+  //*TO-DO: Bind user id when user logs in to maintain a session
+  //        Also bind page number for pagination of questions
+  $scope.userid = 1;
+  $scope.pageNo = 1;
+
+  $scope.quesIArr=[];
+  $scope.quesEArr=[];
+  $scope.ques_e_flag = true;
+  $scope.ques_i_flag = true;
+
+  //*TO-DO: Use constant URLs
+  var url ="http://localhost:8082/v1.0/user/"+$scope.userid+"/interestedquestions/"+$scope.pageNo;
+  var deferred = $q.defer();
+
+  //Fetching expertise topic questions for userid 1
+  $http.get(url)
+	.then(function(api_ques_i_response)
+	{
+    console.log("Int Response: ",api_ques_i_response);
+		deferred.resolve(api_ques_i_response);
+	},
+	function(api_ques_i_response)
+	{
+		deferred.reject(api_ques_i_response);
+	});
+
+	deferred.promise.then(function(api_ques_i_response)
+	{
+		$scope.length = api_ques_i_response.data.length;
+    console.log("Length: ",$scope.length);
+		if($scope.length === 0)
+		{
+			$scope.ques_i_flag = false;
+		}
+		else
+		{
+			$scope.ques_i_flag = true;
+			for(var ind = 0; ind < $scope.length; ind++)
+			{
+				$scope.quesIArr.push({
+					questionid: api_ques_i_response.data[ind].questionid,
+					title: api_ques_i_response.data[ind].title,
+          upvote:  api_ques_i_response.data[ind].upvote,
+          downvote:  api_ques_i_response.data[ind].downvote,
+          state:  api_ques_i_response.data[ind].state,
+          tagnamelist: api_ques_i_response.data[ind].tagnamelist,
+          timestamp:  api_ques_i_response.data[ind].timestamp
+				});
+			}
+		}
+	});
+
+
+  url ="http://localhost:8082/v1.0/user/"+$scope.userid+"/expertisequestions/"+$scope.pageNo;
+  var deferred1 = $q.defer();
+
+  //Fetching expertise topic questions for userid 1
+  $http.get(url)
+	.then(function(api_expertise_response)
+	{
+    deferred1.resolve(api_expertise_response);
+	},
+	function(api_expertise_response)
+	{
+		deferred1.reject(api_expertise_response);
+	});
+
+	deferred1.promise.then(function(api_ques_e_response)
+	{
+		$scope.length = api_ques_e_response.data.length;
+    if($scope.length === 0)
+		{
+			$scope.ques_e_flag = false;
+		}
+		else
+		{
+			$scope.ques_e_flag = true;
+			for(var ind = 0; ind < $scope.length; ind++)
+			{
+				$scope.quesEArr.push({
+					questionid: api_ques_e_response.data[ind].questionid,
+					title: api_ques_e_response.data[ind].title,
+          upvote:  api_ques_e_response.data[ind].upvote,
+          downvote:  api_ques_e_response.data[ind].downvote,
+          state:  api_ques_e_response.data[ind].state,
+          tagnamelist: api_ques_e_response.data[ind].tagnamelist,
+          timestamp:  api_ques_e_response.data[ind].timestamp
+				});
+			}
+		}
+	});
+
+});
